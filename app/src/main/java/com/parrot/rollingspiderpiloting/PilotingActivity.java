77ca0fb0f3abb622ed5907cae2a9ffe6fb1cbea3
Mansimran Singh <me@mansimransingh.com>
2015-04-17 19:50:39 +0100

    test

---
+++ app/src/main/java/com/parrot/rollingspiderpiloting/PilotingActivity.java
@@ -0,0 +1,574 @@
+package com.parrot.rollingspiderpiloting;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryDeviceService;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.Date;
+
+public class PilotingActivity extends Activity implements DeviceControllerListener
+{
+    private static String TAG = PilotingActivity.class.getSimpleName();
+    public static String EXTRA_DEVICE_SERVICE = "pilotingActivity.extra.device.service";
+
+    public DeviceController deviceController;
+    public ARDiscoveryDeviceService service;
+
+    private Button emergencyBt;
+    private Button takeoffBt;
+    private Button landingBt;
+
+    private Button gazUpBt;
+    private Button gazDownBt;
+    private Button yawLeftBt;
+    private Button yawRightBt;
+
+    private Button forwardBt;
+    private Button backBt;
+    private Button rollLeftBt;
+    private Button rollRightBt;
+
+    private Button getData;
+
+    private TextView batteryLabel;
+
+    private AlertDialog alertDialog;
+
+    AsyncHttpClient client;
+
+
+    private int[] instructions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_piloting);
+       client = new AsyncHttpClient();
+
+        final Context ctx = this;
+
+        getData = (Button) findViewById(R.id.getDataBt);
+        getData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                client.get("http://185.28.188.86:3000/bloomberg",new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        // If the response is JSONObject instead of expected JSONArray
+                    }
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+                        // Pull out the first event on the public timeline
+
+                        Toast.makeText(ctx, timeline.toString(), Toast.LENGTH_LONG).show();
+
+                        int prev = 0;
+                        int curr = 0;
+                        instructions = new int[timeline.length()];
+                        for(int i=0; i < timeline.length(); i++){
+
+                            try {
+                                prev = curr;
+                                curr = timeline.getInt(i);
+
+
+
+                                if(curr >= prev){
+                                    up(curr-prev);
+//                                    instructions[i] = curr-prev;
+                                } else {
+                                    down(prev-curr);
+//                                    instructions[i] = prev-curr;
+                                }
+
+                                right();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        run();
+                    }
+                });
+            }
+        });
+
+
+        emergencyBt = (Button) findViewById(R.id.emergencyBt);
+        emergencyBt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                if (deviceController != null)
+                {
+                    deviceController.sendEmergency();
+                }
+            }
+        });
+
+        takeoffBt = (Button) findViewById(R.id.takeoffBt);
+        takeoffBt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                if (deviceController != null)
+                {
+                    deviceController.sendTakeoff();
+                }
+            }
+        });
+        landingBt = (Button) findViewById(R.id.landingBt);
+        landingBt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                if (deviceController != null)
+                {
+                    deviceController.sendLanding();
+                }
+            }
+        });
+
+        gazUpBt = (Button) findViewById(R.id.gazUpBt);
+        gazUpBt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        if (deviceController != null)
+                        {
+                            deviceController.setGaz((byte) 50);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        if (deviceController != null)
+                        {
+                            deviceController.setGaz((byte)0);
+
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        gazDownBt = (Button) findViewById(R.id.gazDownBt);
+        gazDownBt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        if (deviceController != null)
+                        {
+                            deviceController.setGaz((byte)-50);
+
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        if (deviceController != null)
+                        {
+                            deviceController.setGaz((byte)0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+        yawLeftBt = (Button) findViewById(R.id.yawLeftBt);
+        yawLeftBt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        if (deviceController != null)
+                        {
+                            deviceController.setYaw((byte)-50);
+
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        if (deviceController != null)
+                        {
+                            deviceController.setYaw((byte) 0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+        yawRightBt = (Button) findViewById(R.id.yawRightBt);
+        yawRightBt.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        if (deviceController != null)
+                        {
+                            deviceController.setYaw((byte) 50);
+
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        if (deviceController != null)
+                        {
+                            deviceController.setYaw((byte) 0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        forwardBt = (Button) findViewById(R.id.forwardBt);
+        forwardBt.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        if (deviceController != null)
+                        {
+                            deviceController.setPitch((byte) 50);
+                            deviceController.setFlag((byte) 1);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        if (deviceController != null)
+                        {
+                            deviceController.setPitch((byte) 0);
+                            deviceController.setFlag((byte) 0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+        backBt = (Button) findViewById(R.id.backBt);
+        backBt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        if (deviceController != null)
+                        {
+                            deviceController.setPitch((byte)-50);
+                            deviceController.setFlag((byte)1);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        if (deviceController != null)
+                        {
+                            deviceController.setPitch((byte)0);
+                            deviceController.setFlag((byte)0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+        rollLeftBt = (Button) findViewById(R.id.rollLeftBt);
+        rollLeftBt.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        if (deviceController != null)
+                        {
+                            deviceController.setRoll((byte) -50);
+                            deviceController.setFlag((byte) 1);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        if (deviceController != null)
+                        {
+                            deviceController.setRoll((byte) 0);
+                            deviceController.setFlag((byte) 0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+        rollRightBt = (Button) findViewById(R.id.rollRightBt);
+        rollRightBt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        if (deviceController != null)
+                        {
+                            deviceController.setRoll((byte)50);
+                            deviceController.setFlag((byte)1);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        if (deviceController != null)
+                        {
+                            deviceController.setRoll((byte)0);
+                            deviceController.setFlag((byte)0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        batteryLabel = (TextView) findViewById(R.id.batteryLabel);
+
+        Intent intent = getIntent();
+        service = intent.getParcelableExtra(EXTRA_DEVICE_SERVICE);
+
+        deviceController = new DeviceController(this, service);
+        deviceController.setListener(this);
+
+
+    }
+
+
+    private void run(){
+
+    }
+
+    private void up(int i){
+        if (deviceController != null)
+        {
+            deviceController.setGaz((byte) (2 * i));
+        }
+    }
+
+    private void down(int i){
+        if (deviceController != null)
+        {
+            deviceController.setGaz((byte) (-2 * i));
+        }
+    }
+
+    private void right(){
+        if (deviceController != null)
+        {
+            deviceController.setRoll((byte)5);
+            deviceController.setFlag((byte)1);
+        }
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+
+        if (deviceController != null)
+        {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(PilotingActivity.this);
+
+            // set title
+            alertDialogBuilder.setTitle("Connecting ...");
+
+            // create alert dialog
+            alertDialog = alertDialogBuilder.create();
+
+            // show it
+            alertDialog.show();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run()
+                {
+                    boolean failed = false;
+
+                    failed = deviceController.start();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run()
+                        {
+                            //alertDialog.hide();
+                            alertDialog.dismiss();
+                        }
+                    });
+
+                    if (failed)
+                    {
+                        finish();
+                    }
+                    else
+                    {
+                        //only with RollingSpider in version 1.97 : date and time must be sent to permit a reconnection
+                        Date currentDate = new Date(System.currentTimeMillis());
+                        deviceController.sendDate(currentDate);
+                        deviceController.sendTime(currentDate);
+                    }
+                }
+            }).start();
+
+        }
+    }
+
+    private void stopDeviceController()
+    {
+        if (deviceController != null)
+        {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(PilotingActivity.this);
+
+            // set title
+            alertDialogBuilder.setTitle("Disconnecting ...");
+
+            // create alert dialog
+            alertDialog = alertDialogBuilder.create();
+
+            // show it
+            alertDialog.show();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run()
+                {
+                    deviceController.stop();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run()
+                        {
+                            //alertDialog.hide();
+                            alertDialog.dismiss();
+                            finish();
+                        }
+                    });
+
+                }
+            }).start();
+        }
+    }
+
+    @Override
+    protected void onStop()
+    {
+        stopDeviceController();
+
+        super.onStop();
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        stopDeviceController();
+    }
+
+    @Override
+    public void onDisconnect()
+    {
+        stopDeviceController();
+    }
+
+    @Override
+    public void onUpdateBattery(final byte percent)
+    {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run()
+            {
+                batteryLabel.setText(String.format("%d%%", percent));
+            }
+        });
+
+    }
+}
